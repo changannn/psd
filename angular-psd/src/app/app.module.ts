4f47bb310520b5832e7d186c7183a569993dd14b
yyueda <yyuedaa@gmail.com>
2023-10-13 15:08:18 +0800

    Add home component and edit format of index.html

--- angular-psd/src/app/app.module.ts
+++ angular-psd/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './Login/login.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
