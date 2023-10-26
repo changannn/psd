ad4ddf91138125a350a1d7449f5f60125abb1efe
2837329a <2837329a@student.gla.ac.uk>
2023-10-26 23:03:31 +0800

    Connected login frontend to backend
    -Need to inspect page to see the console.log() messages
    
    -Successful login will see status 200
    -Unsuccessful login will see status 401

--- angular-psd/src/app/Login/login.component.ts
+++ angular-psd/src/app/Login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string | undefined;
+  password: string | undefined;
+  role = 'admin';
+  apiUrl = 'http://localhost:8080';
 
+  constructor(private http: HttpClient) {}
+
+  login() {
+    const loginDetails = {
+      username: this.username,
+      password: this.password,
+      role: this.role
+    };
+    this.http.post(`${this.apiUrl}/login`, loginDetails).subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
--- backend/src/main/java/com/psd/backend/model/User.java
+++ backend/src/main/java/com/psd/backend/model/User.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "userTable")
+@Table(name = "user_table")
 public class User {
 
     @Id
