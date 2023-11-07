b16fca7cb8775ebd55e195bedcbb8598baabb0b3
relmoo1220 <gamingwithmura@gmail.com>
2023-11-07 13:17:21 +0800

    Update registration.service.ts

--- angular-psd/src/app/registration.service.ts
+++ angular-psd/src/app/registration.service.ts
@@ -10,7 +10,7 @@ export class RegistrationService {
   constructor(private http: HttpClient) {}
 
   registerUser(userData: any) {
-    return this.http.post(`${this.apiUrl}/add`, userData);
+    return this.http.post(`${this.apiUrl}/register`, userData);
   }
 }
 
