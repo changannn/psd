74d19f0e2e077c9cb8837a73296cf3193c95398f
nkjcolin <colinngkj254@gmail.com>
2023-11-11 23:10:55 +0800

    clean up unused import

--- backend/src/main/java/com/psd/backend/controller/UserController.java
+++ backend/src/main/java/com/psd/backend/controller/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.psd.backend.model.FrontendResponse;
 import com.psd.backend.model.User;
 import com.psd.backend.service.UserService;
 
