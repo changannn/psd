28c5dac574d72ddbf32a2cc532e766f7501054ea
nkjcolin <colinngkj2549@gmail.com>
2023-11-11 22:57:52 +0800

    update cors filter

--- backend/src/main/java/com/psd/backend/config/CorsConfiguration.java
+++ backend/src/main/java/com/psd/backend/config/CorsConfiguration.java
@@ -2,6 +2,8 @@ package com.psd.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,7 +15,14 @@ public class CorsConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**"); // Enable CORS for the whole application
+                registry.addMapping("/**")                         // Enable CORS for the whole application
+                        .allowedOrigins("http://localhost:4200")    // Only takes request from angular
+                        .allowedMethods(HttpMethod.GET.name(),
+                                        HttpMethod.POST.name(),
+                                        HttpMethod.PUT.name(),           
+                                        HttpMethod.DELETE.name())
+                        .allowedHeaders(HttpHeaders.CONTENT_TYPE,
+                                        HttpHeaders.AUTHORIZATION); 
             }
         };
     }
--- backend/src/main/java/com/psd/backend/controller/UserController.java
+++ backend/src/main/java/com/psd/backend/controller/UserController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.psd.backend.service.JwtService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +18,6 @@ import com.psd.backend.model.User;
 import com.psd.backend.service.UserService;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
     
     private final UserService userService;
