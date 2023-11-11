88ff66a0b4f0b7ac4178568eb521c2a1f3db2c14
nkjcolin <colinngkj2549@gmail.com>
2023-11-11 22:37:52 +0800

    cleanup import

--- backend/src/main/java/com/psd/backend/config/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/psd/backend/config/JwtAuthenticationFilter.java
@@ -9,7 +9,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.web.SecurityContextDsl;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
