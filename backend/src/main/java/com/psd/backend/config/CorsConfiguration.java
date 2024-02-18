b7f8dd6a808a981fb1e50eaea7ea10a89a38f79e
nkjcolin <colinngkj2549@gmail.com>
2024-02-18 12:11:54 +0800

    accept all incoming from api gateway

--- backend/src/main/java/com/psd/backend/config/CorsConfiguration.java
+++ backend/src/main/java/com/psd/backend/config/CorsConfiguration.java
@@ -16,7 +16,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")                         // Enable CORS for the whole application
-                        .allowedOrigins("https://u-saas.wizvision.com/")    // Only takes request from angular
+                        .allowedOrigins("*")    // Only takes request from angular
                         .allowedMethods(HttpMethod.GET.name(),
                                         HttpMethod.POST.name(),
                                         HttpMethod.PUT.name(),           
