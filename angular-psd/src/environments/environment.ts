e5c993b78de727cbd68b58b8a95ef647d0d6db2c
nkjcolin <colinngkj2549@gmail.com>
2024-02-18 12:11:17 +0800

    aws api gateway endpoint

--- angular-psd/src/environments/environment.ts
+++ angular-psd/src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production: false,
     title: 'angular-psd',
-    apiURL: 'http://localhost:8080'
+    // apiURL: 'http://localhost:8080'
+    apiURL: 'https://qkfholp62c.execute-api.ap-southeast-1.amazonaws.com/dev'
+    
   };
