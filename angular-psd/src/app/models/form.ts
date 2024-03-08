1a485cb83409233d26677c3260df7cf5a1740e7f
nkjcolin <colinngkj2549@gmail.com>
2024-03-08 22:23:27 +0800

    add form model

---
+++ angular-psd/src/app/models/form.ts
@@ -0,0 +1,39 @@
+export interface Form {
+    id: number;
+    username: string;
+    userMode: string;
+    projectType: string;
+    processors: number;
+    materials: string;
+    rgbValue: string;
+    cityLocation: string;
+    meshResolutionSolar: string;
+    meshOffset: string;
+    simulationTypes: string;
+    selectedSimulationType: string;
+    solarIrradiationCheckbox: boolean;
+    absorbedSolarEnergyCheckbox: boolean;
+    solarShadingCheckbox: boolean;
+    daylightIlluminanceCheckbox: boolean;
+    absorbedHeatFluxCheckbox: boolean;
+    skyViewFactorCheckbox: boolean;
+    startDate: Date;
+    endDate: Date;
+    numOfReflections: number;
+    numOfSamplingRays: number;
+    sunInclusion: string;
+    multiband: string;
+    receiverGrid: string;
+    receiverOffset: string;
+    meshResolutionNoise: string;
+    roadCategory: string;
+    inputTypes: string;
+    selectedInputType: string;
+    inputValue: string;
+    materialAbsorption: string;
+    numOfVehicle: string;
+    vehicleSpeed: string;
+    heavyVehicle: string;
+    createdAt: Date;
+}
+
