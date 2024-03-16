c863e504f927a58963147bfa1fa426804d0cee57
jun837 <143344073+junl837@users.noreply.github.com>
2024-03-16 22:04:50 +0800

    Include apiHeaders in http posts

--- angular-psd/src/app/services/login.service.ts
+++ angular-psd/src/app/services/login.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AuthenticationRequest } from '../models/authentication-request';
 import { AuthenticationResponse } from '../models/authentication-response';
 import { VerificationRequest } from '../models/verification-request';
@@ -10,14 +10,17 @@ import { environment } from '../../environments/environment';
 })
 export class LoginService {
   private apiUrl = environment.apiURL; // Replace with the URL of spring in environment folder
+  private apiHeaders = new HttpHeaders({
+    'x-api-key': environment.apiKey
+  });
 
   constructor(private http: HttpClient) { }
 
   loginUser(authenticationRequest: AuthenticationRequest) {
-    return this.http.post<AuthenticationResponse>(`${this.apiUrl}/auth/authenticate`, authenticationRequest);
+    return this.http.post<AuthenticationResponse>(`${this.apiUrl}/auth/authenticate`, authenticationRequest, { headers: this.apiHeaders });
   }
 
   verifyCode(verificationRequest: VerificationRequest) {
-    return this.http.post<AuthenticationResponse>(`${this.apiUrl}/auth/verify`, verificationRequest);
+    return this.http.post<AuthenticationResponse>(`${this.apiUrl}/auth/verify`, verificationRequest, { headers: this.apiHeaders });
   }
 }
